258aafc006d52420224cfe334b49c2b4fc39beba
Sam Mercer <smercer10@proton.me>
2024-07-19 17:16:11 +0100

    Create memory interface

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,5 @@ const std = @import("std");
 const memory = @import("memory.zig");
 
 pub fn main() void {
-    std.debug.print("LVM-3 has {d} addressable memory locations.\n", .{memory.max});
+    std.debug.print("LVM-3 has {d} addressable memory locations.\n", .{memory.size});
 }
--- src/memory.zig
+++ src/memory.zig
@@ -1,4 +1,15 @@
 const std = @import("std");
 
-/// Maximum addressable memory locations.
-pub const max = std.math.maxInt(u16);
+// Number of addressable memory locations.
+pub const size = std.math.maxInt(u16);
+
+var memory = [_]u16{0} ** size;
+
+/// Read the value at a memory address.
+pub fn read(addr: u16) u16 {
+    return memory[addr];
+}
+/// Write a value to a memory address.
+pub fn write(addr: u16, val: u16) void {
+    memory[addr] = val;
+}
