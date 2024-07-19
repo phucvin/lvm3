7eeb959c04e068bf5d3c323a57e0c8fdb6e80c04
Sam Mercer <smercer10@proton.me>
2024-07-19 16:01:10 +0100

    Set up project

---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const memory = @import("memory.zig");
+
+pub fn main() void {
+    std.debug.print("LVM-3 has {d} addressable memory locations.\n", .{memory.max});
+}
---
+++ src/memory.zig
@@ -0,0 +1,4 @@
+const std = @import("std");
+
+/// Maximum addressable memory locations.
+pub const max = std.math.maxInt(u16);
