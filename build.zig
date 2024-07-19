95ee9584163492d1b6afa7f976fc8e868130bf83
Sam Mercer <smercer10@proton.me>
2024-07-19 18:56:44 +0100

    Test memory read and write

--- build.zig
+++ build.zig
@@ -17,4 +17,15 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/unit_tests.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    const test_step = b.step("test", "Run all the tests");
+    test_step.dependOn(&run_unit_tests.step);
 }
--- src/memory.zig
+++ src/memory.zig
@@ -6,10 +6,41 @@ pub const size = std.math.maxInt(u16);
 var memory = [_]u16{0} ** size;
 
 /// Read the value at a memory address.
-pub fn read(addr: u16) u16 {
+pub fn read(addr: u16) !u16 {
+    if (addr == size) {
+        return error.OutOfBounds;
+    }
     return memory[addr];
 }
 /// Write a value to a memory address.
-pub fn write(addr: u16, val: u16) void {
+pub fn write(addr: u16, val: u16) !void {
+    if (addr == size) {
+        return error.OutOfBounds;
+    }
     memory[addr] = val;
+}
+
+test "memory read and write" {
+    comptime var addr = 0;
+    comptime var val = 42;
+    try write(addr, val);
+    try std.testing.expectEqual(val, read(addr));
+
+    addr = 1000;
+    val = 123;
+    try write(addr, val);
+    try std.testing.expectEqual(val, read(addr));
+    try std.testing.expectEqual(0, read(addr - 1));
+    try std.testing.expectEqual(0, read(addr + 1));
+
+    addr = size - 1;
+    val = size;
+    try write(addr, val);
+    try std.testing.expectEqual(val, read(addr));
+
+    addr = size;
+    val = 0;
+    try std.testing.expectError(error.OutOfBounds, write(addr, val));
+    try write(addr - 1, val);
+    try std.testing.expectError(error.OutOfBounds, read(addr));
 }
