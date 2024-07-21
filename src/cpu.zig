6165b5f02569a84bdb9a1e7cfee4426485d00e8d
Sam Mercer <smercer10@proton.me>
2024-07-21 13:40:34 +0100

    Create opcode enum

---
+++ src/cpu.zig
@@ -0,0 +1,19 @@
+/// Instruction opcodes.
+pub const Op = enum {
+    br, // Branch.
+    add, // Add.
+    ld, // Load.
+    st, // Store.
+    jsr, // Jump register.
+    and_, // Bitwise AND.
+    ldr, // Load register.
+    str, // Store register.
+    rti, // Return from interrupt (unused).
+    not, // Bitwise NOT.
+    ldi, // Load indirect.
+    sti, // Store indirect.
+    jmp, // Jump.
+    res, // Reserved (unused).
+    lea, // Load effective address.
+    trap, // Execute trap.
+};
